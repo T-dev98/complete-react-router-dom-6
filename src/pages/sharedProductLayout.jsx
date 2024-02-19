add5923cf37acaea10aa95afbb83ead27d7af663
T-dev98 <133159152+T-dev98@users.noreply.github.com>
2024-02-19 11:33:42 -0500

    Outlet component

---
+++ src/pages/sharedProductLayout.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom"
+
+const SharedProductLayout = () => {
+    return(
+        <>
+        <Outlet />
+        </>
+    )
+}
+export default SharedProductLayout;
